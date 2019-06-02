5de4e0c9f036f2c93f8c125a22ccaf42b5e6b830
Vlad Golitsyn <vladgolitsyn@gmail.com>
2019-06-02 13:20:07 -0400

    Updates

---
+++ precache-manifest.128f31714a9f57c1abc3d86bf0589d80.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5743d44a63581db60d311f57991f9ef9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/clicky-game/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "8c8d780059f98379948b",
+    "url": "/clicky-game/static/js/main.3c09b037.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
